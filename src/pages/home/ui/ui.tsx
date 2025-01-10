b3283a30d593b7aff44e3db7c04f2758b3d99ac9
AlexeyBMSTU <nap8099@gmail.com>
2025-01-11 02:04:47 +0300

    Добавил скролл

--- src/pages/home/ui/ui.tsx
+++ src/pages/home/ui/ui.tsx
@@ -28,41 +28,43 @@ const MobileHome = () => {
               <div className='paginate-item active'></div>
             </div>
           </div>
-          <div className='events'>
-            <div className='event mobile'>
-              <h3>2015</h3>
-              <p>
-                13 сентября — частное солнечное затмение, видимое в Южной Африке
-                и части Антарктиды
-              </p>
-            </div>
-            <div className='event mobile'>
-              <h3>2016</h3>
-              <p>
-                Телескоп «Хаббл» обнаружил самую удалённую из всех обнаруженных
-                галактик, получившую обозначение GN-z11
-              </p>
-            </div>
-            <div className='event mobile'>
-              <h3>2017</h3>
-              <p>
-                Компания Tesla официально представила первый в мире
-                электрический грузовик Tesla Semi
-              </p>
-            </div>
-            <div className='event mobile'>
-              <h3>2017</h3>
-              <p>
-                Компания Tesla официально представила первый в мире
-                электрический грузовик Tesla Semi
-              </p>
-            </div>
-            <div className='event mobile'>
-              <h3>2017</h3>
-              <p>
-                Компания Tesla официально представила первый в мире
-                электрический грузовик Tesla Semi
-              </p>
+          <div className='interactive-info'>
+            <div className='events'>
+              <div className='event mobile'>
+                <h3>2015</h3>
+                <p>
+                  13 сентября — частное солнечное затмение, видимое в Южной
+                  Африке и части Антарктиды
+                </p>
+              </div>
+              <div className='event mobile'>
+                <h3>2016</h3>
+                <p>
+                  Телескоп «Хаббл» обнаружил самую удалённую из всех
+                  обнаруженных галактик, получившую обозначение GN-z11
+                </p>
+              </div>
+              <div className='event mobile'>
+                <h3>2017</h3>
+                <p>
+                  Компания Tesla официально представила первый в мире
+                  электрический грузовик Tesla Semi
+                </p>
+              </div>
+              <div className='event mobile'>
+                <h3>2017</h3>
+                <p>
+                  Компания Tesla официально представила первый в мире
+                  электрический грузовик Tesla Semi
+                </p>
+              </div>
+              <div className='event mobile'>
+                <h3>2017</h3>
+                <p>
+                  Компания Tesla официально представила первый в мире
+                  электрический грузовик Tesla Semi
+                </p>
+              </div>
             </div>
           </div>
         </div>
